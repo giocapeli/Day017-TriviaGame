36006934f49939f89b4f384d0c22d5ada518ca64
Giovanni Capeli <capeli.andreas@gmail.com>
2023-02-11 15:19:47 +0100

    fixing input

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from questions import TriviaQuestions
 
 questions_list = TriviaQuestions()
 play_again = True
+letter_choices = ["A", "B", "C", "D", "E"]
+
 
 print("Welcome to the Trivia Game!\nThis game uses data from https://opentdb.com/\n")
 
 questions_list.init_trivia()
 
 while play_again:
+    guess = ''
     for n in range(1, 11):
         questions_list.get_next_question()
-        guess = input(f'Your answer: ').upper().split()[0]
+        while guess == '' or guess not in letter_choices:
+            guess = input(f'Your answer: ').upper().split()
         questions_list.check_answer(guess)
 
     print(f'Total score: {questions_list.score}')
