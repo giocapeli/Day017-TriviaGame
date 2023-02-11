3a412dbad3724873627604b562b985997a1e2d2c
Giovanni Capeli <capeli.andreas@gmail.com>
2023-02-11 11:51:21 +0100

    questions

--- modules/questions.py
+++ modules/questions.py
@@ -1,6 +1,9 @@
 import request_data
 import random
 import messages
+
+letter_choices = ["A", "B", "C", "D", "E"]
+
 class TriviaQuestions:
 
     def __init__(self):
@@ -10,6 +13,7 @@ class TriviaQuestions:
         self.current_options = 0
         self.correct_answer = 0
         self.score = 0
+        self.question_n = 1
 
     def init_trivia(self):
         self.data = request_data.Questions()
@@ -31,13 +35,23 @@ class TriviaQuestions:
         self.current_options = self.questions[0]['incorrect_answers']
         self.current_options.append(self.correct_answer)
         random.shuffle(self.current_options)
+        self.print_question(self)
+        self.question_n += 1
+
+    def print_question(self):
+        print(f'Question {self.question_n}: {self.current_question}')
+        print(f'Options:')
+        for i, o in enumerate(self.current_options):
+            print(f'[{letter_choices[i]}] {o}')
 
     def check_answer(self, guess):
-        print(f'You guessed: {guess}')
-        if guess == self.correct_answer:
+        index = letter_choices.index(guess)
+        print(f'You guessed: {guess}, {self.current_options[index]}')
+        if self.current_options[index] == self.correct_answer:
             self.score += 1
             messages.get_message(self.questions[0], True)
         else:
             messages.get_message(self.questions[0], False)
+        print(f'Your score until now: {questions_list.score} of {n}\n')
         self.questions.pop(0)
 
