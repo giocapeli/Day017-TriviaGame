2d7e9a6bdc631f6a73be42767f957511ee59461c
Giovanni Capeli <capeli.andreas@gmail.com>
2023-02-21 19:43:17 +0100

    interface class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from questions import TriviaQuestions
+import ui
 
 questions_list = TriviaQuestions()
 play_again = True
@@ -9,17 +10,19 @@ print("Welcome to the Trivia Game!\nThis game uses data from https://opentdb.com
 
 questions_list.init_trivia()
 
-while play_again:
-    for n in range(1, 11):
-        guess = ''
-        questions_list.get_next_question()
-        while guess == '' or guess not in letter_choices:
-            guess = input(f'Your answer: ').upper()
-        questions_list.check_answer(guess)
+quiz_ui = ui.Interface()
 
-    print(f'Total score: {questions_list.score}')
-    continue_playing = input("Play again? (Y/N): \n").lower()
-    if continue_playing == 'n':
-        play_again = False
+# while play_again:
+#     for n in range(1, 11):
+#         guess = ''
+#         questions_list.get_next_question()
+#         while guess == '' or guess not in letter_choices:
+#             guess = input(f'Your answer: ').upper()
+#         questions_list.check_answer(guess)
+
+#     print(f'Total score: {questions_list.score}')
+#     continue_playing = input("Play again? (Y/N): \n").lower()
+#     if continue_playing == 'n':
+#         play_again = False
 
 print("Thank you for playing!")
---
+++ ui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+class Interface:
+    
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizz Game")
+        
+        self.window.mainloop()
